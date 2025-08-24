612cb706bbd45ba6dba9130f57ea987f43debf3d
Raj <rajdataengineer55@gmail.com>
2025-08-24 06:48:38 +0000

    rajdataengineer55@cloudshell:~ (localpulse-9e3lz)$ gsutil ls gs://gcf-v2

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCgJBVTnX0rl4GVTMIccw9IY6U9hzZKRvA",
   authDomain: "localpulse-9e3lz.firebaseapp.com",
   projectId: "localpulse-9e3lz",
-  storageBucket: "localpulse-9e3lz.appspot.com",
+  storageBucket: "localpulse-9e3lz.firebasestorage.app",
   messagingSenderId: "542848805293",
   appId: "1:542848805293:web:99ae91ab88f7723b1f5924",
   measurementId: "G-5Y6R706M5Y"
