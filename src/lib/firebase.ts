f8ba8bd537c93b3a303a7e59c747b191f33a3d87
Raj <rajdataengineer55@gmail.com>
2025-08-25 18:32:32 +0000

    leave image favicon one we are checking mainly if add posted follower ca

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,16 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
 
 // Your production Firebase project configuration.
 const firebaseConfig = {
   apiKey: "AIzaSyCgJBVTnX0rl4GVTMIccw9IY6U9hzZKRvA",
   authDomain: "localpulse-9e3lz.firebaseapp.com",
   projectId: "localpulse-9e3lz",
-  storageBucket: "localpulse-9e3lz.firebasestorage.app",
+  storageBucket: "localpulse-9e3lz.appspot.com",
   messagingSenderId: "542848805293",
   appId: "1:542848805293:web:99ae91ab88f7723b1f5924",
   measurementId: "G-5Y6R706M5Y"
@@ -23,6 +23,18 @@ const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseCon
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
+
+// If we are in a development environment, connect to the local emulators
+if (process.env.NODE_ENV === 'development') {
+    console.log("Development environment detected. Connecting to Firebase emulators.");
+    // Point Firestore to the local emulator
+    connectFirestoreEmulator(db, '127.0.0.1', 8080);
+    // Point Auth to the local emulator
+    connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
+    // Point Storage to the local emulator
+    connectStorageEmulator(storage, '127.0.0.1', 9199);
+}
+
 
 export { db, storage, auth };
 export default app;
