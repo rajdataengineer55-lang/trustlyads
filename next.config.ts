55952e9f91c1c79eb88759bd1a3963b144c503a0
Raj <rajdataengineer55@gmail.com>
2025-08-27 09:15:11 +0000

    in dev images are loading but production is not loading please identify

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
-
-import { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
@@ -7,6 +6,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
