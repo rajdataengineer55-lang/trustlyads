96ae3ba0ef74bdfaeeaad9085beb39af49d7b88b
Raj <rajdataengineer55@gmail.com>
2025-08-28 16:27:36 +0000

    Invalid src prop (https://firebasestorage.googleapis.com/v0/b/localpulse

---
+++ src/next.config.ts
@@ -0,0 +1,21 @@
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "/v0/b/localpulse-9e3lz.appspot.com/o/**",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
