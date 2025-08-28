67631460a79d6295e426d1cc54ea79ea6a3fb87b
Raj <rajdataengineer55@gmail.com>
2025-08-28 06:44:17 +0000

    1. ⚠️ Warning about allowedDevOrigins
    This one is not blocking right no

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,16 @@ const nextConfig = {
         hostname: "firebasestorage.googleapis.com",
         pathname: "/v0/b/localpulse-9e3lz.appspot.com/o/**",
       },
-       {
+      {
         protocol: "https",
         hostname: "placehold.co",
       },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      "http://localhost:9003",
+      "https://9003-firebase-studio-1755798338059.cluster-fkltigo73ncaixtmokrzxhwsfc.cloudworkstations.dev"
     ],
   },
 };
