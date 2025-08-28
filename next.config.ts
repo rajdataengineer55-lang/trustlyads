bad40d75edb3b3e885308da25b8ee7715837a855
Raj <rajdataengineer55@gmail.com>
2025-08-28 10:48:07 +0000

    first check wht is the image url where it is loading please check once

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "placehold.co", // keep if you use placeholder images
+        hostname: "placehold.co",
         pathname: "/**",
       },
     ],
