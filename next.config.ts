ce500654f1987a25fafc708d6bd6c01b161c337a
Raj <rajdataengineer55@gmail.com>
2025-08-28 06:11:58 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <FeaturedOffers so

--- next.config.ts
+++ next.config.ts
@@ -3,38 +3,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "placehold.co",
-        port: "",
-        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "firebasestorage.googleapis.com",
-        port: "",
-        pathname: "/**",
       },
-      {
-        protocol: "https",
-        hostname: "www.trustlyads.in",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "trustlyads.in",
-        port: "",
-        pathname: "/**",
-      }
     ],
   },
 
