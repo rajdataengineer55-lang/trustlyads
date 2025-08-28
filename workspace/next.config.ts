9052c098915ad42335ad42ccc40cc7d08d4f587a
Raj <rajdataengineer55@gmail.com>
2025-08-28 09:26:15 +0000

    Error: Invalid src prop (https://firebasestorage.googleapis.com/v0/b/loc

---
+++ workspace/next.config.ts
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "/v0/b/localpulse-9e3lz.appspot.com/o/**",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        pathname: "/**",
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      "http://localhost:9003",
+      "https://9003-firebase-studio-1755798338059.cluster-fkltigo73ncaixtmokrzxhwsfc.cloudworkstations.dev",
+    ],
+  },
+};
+
+export default nextConfig;
