2fad6097ae7745a493720513209dbd852d99ce2d
Raj <rajdataengineer55@gmail.com>
2025-09-01 04:43:33 +0000

    Install manually Recommended Below is the Google tag for this account. C

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 
 import type { Metadata, Viewport } from 'next';
 import { Open_Sans } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { OffersProvider } from '@/contexts/OffersContext';
@@ -38,6 +39,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-5Y6R706M5Y"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-5Y6R706M5Y');
+          `}
+        </Script>
+      </head>
       <body className={cn("antialiased font-sans", openSans.variable)}>
         <ThemeProvider
             attribute="class"
